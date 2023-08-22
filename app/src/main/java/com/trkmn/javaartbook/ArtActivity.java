bf577583293e482d42a65a0a9921d691bb7649a0
Uğur Türkmen <trkmn.ugur.12@gmail.com>
2023-08-23 00:52:40 +0300

    view binding

--- app/src/main/java/com/trkmn/javaartbook/ArtActivity.java
+++ app/src/main/java/com/trkmn/javaartbook/ArtActivity.java
@@ -1,16 +1,29 @@
 package com.trkmn.javaartbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.trkmn.javaartbook.databinding.ActivityArtBinding;
+
 public class ArtActivity extends AppCompatActivity {
+
+    private ActivityArtBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_art);
+        binding = ActivityArtBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
     }
 
     public void save(View view){
@@ -18,6 +31,26 @@ public class ArtActivity extends AppCompatActivity {
     }
 
     public void selectImage(View view){
+
+        //İzinleri kontrol etmek
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }).show();
+            }else{
+                //request permission
+
+            }
+
+
+        }else{
+            //gallery
+        }
 
     }
 }
--- app/src/main/java/com/trkmn/javaartbook/MainActivity.java
+++ app/src/main/java/com/trkmn/javaartbook/MainActivity.java
@@ -2,13 +2,33 @@ package com.trkmn.javaartbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.trkmn.javaartbook.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ArtActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

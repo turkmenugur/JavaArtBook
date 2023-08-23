bef1c349d037226fa6be47be2a581406c0033775
Uğur Türkmen <trkmn.ugur.12@gmail.com>
2023-08-23 15:46:22 +0300

    Launchers

--- app/src/main/java/com/trkmn/javaartbook/ArtActivity.java
+++ app/src/main/java/com/trkmn/javaartbook/ArtActivity.java
@@ -1,13 +1,24 @@
 package com.trkmn.javaartbook;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.trkmn.javaartbook.databinding.ActivityArtBinding;
@@ -16,6 +27,17 @@ public class ArtActivity extends AppCompatActivity {
 
     private ActivityArtBinding binding;
 
+    /*Aktivite sonuç başlatıcı ->
+      Biz galeriye gidip ordan resim seçip onu ele almak mı istiyoruz
+      Biz bir izin isteyip o izin verildiğinde ne oalcağını yazmak mı istiyoruz
+      Hepsinde ActivityResultLauncher kullanacağız
+     */
+    ActivityResultLauncher<Intent> activityResultLauncher; //Galeriye gitmek için
+    ActivityResultLauncher<String> permissionLauncher; //izinlerde kullanacağız
+    //Bunları onCreate altında register etmemiz gerekiyor
+
+    Bitmap selectedImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +45,7 @@ public class ArtActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-
+        registerLauncher();
     }
 
     public void save(View view){
@@ -31,26 +53,104 @@ public class ArtActivity extends AppCompatActivity {
     }
 
     public void selectImage(View view){
+        //Android Tiramisu API 33Ten sonra gelen READ_MEDIA_IMAGES izni geldiği için bu kontrolü eklememiz gerekiyor
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            //Android 33+ -> READ_MEDIA_IMAGES
+            //İzinleri kontrol etmek
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED){
 
-        //İzinleri kontrol etmek
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_MEDIA_IMAGES)){
+                    Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            //request permission
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES);
+                        }
+                    }).show();
+                }else{
+                    //request permission
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES);
+                }
 
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
-                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                    }
-                }).show();
             }else{
-                //request permission
-
+                //gallery
+                Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                activityResultLauncher.launch(intentToGallery);
             }
-
-
         }else{
-            //gallery
-        }
+            //Android 32- -> READ_EXTERNAL_STORAGE
+            //İzinleri kontrol etmek
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
 
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                    Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            //request permission
+                            permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+                        }
+                    }).show();
+                }else{
+                    //request permission
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+                }
+
+            }else{
+                //gallery
+                Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                activityResultLauncher.launch(intentToGallery);
+            }
+        }
+    }
+
+    private void registerLauncher(){
+        //Galeriye gitmek
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK){//Kullanıcı bir şey seçti
+                    Intent intentFromResult = result.getData();
+                    if (intentFromResult != null){
+                        //Bu getData kullanıcının seçtiği görselin nerede kayıtlı olduğunu veriyor
+                        Uri imageData =  intentFromResult.getData();
+
+                        //Bize kullanıcının resminin verisi lazım olduğu için bitmaple işlemi yapıyoruz. Çünkü o veriiyi veri tabanına kaydedeceğiz
+                        //binding.imageView.setImageURI(imageData);
+
+                        //Görseli Bitmap'e çevirme
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28){
+                                //Bu işlem sadece API 28 ve üzerinde çalışıyor. Bu yüzden bu kontrolü yapıyoruz
+                                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(),imageData);
+                                selectedImage = ImageDecoder.decodeBitmap(source);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }else {
+                                selectedImage = MediaStore.Images.Media.getBitmap(ArtActivity.this.getContentResolver(), imageData);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+
+        //İzni isteme
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if (result){
+                    //Permission granted
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    //gallery
+                    activityResultLauncher.launch(intentToGallery);
+
+                }else {
+                    //permission denied
+                    Toast.makeText(ArtActivity.this, "Permission needed!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
